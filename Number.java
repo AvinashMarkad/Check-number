75535cc67f92212365ab19a7012ee3a1ebfa3258
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2022-03-09 20:06:49 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Number
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the any Number:");
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.println(a+" Is Positiv Number");
+}
+else
+{
+System.out.println(a+" Is Negativ Number");
+}
+}
+} 
